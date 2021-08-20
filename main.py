b02e44c46acd8d9dc44164ba63865b3f42db9bce
jackjack3006 <jvsanoj23@gmail.com>
2021-08-20 10:50:39 +0530

    Adding coordinates(not yet right)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 
 image = cv2.imread('example_page-0001.jpg')
+coordinates = [[0, 0]]
 result = image.copy()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower = np.array([155,25,0])
@@ -9,17 +10,14 @@ upper = np.array([179,255,255])
 mask = cv2.inRange(image, lower, upper)
 result = cv2.bitwise_and(result, result, mask=mask)
 
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+_, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for pic, contour in enumerate(contours):
     x, y, w, h = cv2.boundingRect(contour)
-    cv2.rectangle(image, (x, y),
-                    (x + w, y + h),
-                    (25, 255, 255), 2)
-
-    cv2.putText(image, "Orange", (x, y),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1.0, (255, 255, 0))
+    cv2.rectangle(image, (x, y), (x + w, y + h), (25, 255, 255), 2)
+    centre_x, centre_y = ((x * 2 + w) // 2, (y * 2 + h) // 2)
+    coordinates.append([centre_x, centre_y])
+    print(coordinates)
 
 result = cv2.resize(result, (360, 500))
 
