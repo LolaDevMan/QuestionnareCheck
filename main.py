04fc839a6a9f4dbda39cd4806bd297f7f8b3c161
jackjack3006 <jvsanoj23@gmail.com>
2021-08-20 10:13:59 +0530

    Checking the area

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ upper = np.array([179,255,255])
 mask = cv2.inRange(image, lower, upper)
 result = cv2.bitwise_and(result, result, mask=mask)
 
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for pic, contour in enumerate(contours):
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(image, (x, y),
+                    (x + w, y + h),
+                    (25, 255, 255), 2)
+
+    cv2.putText(image, "Orange", (x, y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1.0, (255, 255, 0))
+
 result = cv2.resize(result, (360, 500))
 
 cv2.imshow('mask', mask)
