9edebb9cc78eb573c7aa3306013ac8bc48aa40f1
jackjack3006 <jvsanoj23@gmail.com>
2021-08-20 11:02:35 +0530

    Getting coordinates

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 
 image = cv2.imread('example_page-0001.jpg')
-coordinates = [[0, 0]]
+# image = image[190:450, 10:1290]
+coordinates = []
+count = 0
 result = image.copy()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower = np.array([155,25,0])
@@ -13,14 +15,21 @@ result = cv2.bitwise_and(result, result, mask=mask)
 _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for pic, contour in enumerate(contours):
-    x, y, w, h = cv2.boundingRect(contour)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (25, 255, 255), 2)
-    centre_x, centre_y = ((x * 2 + w) // 2, (y * 2 + h) // 2)
-    coordinates.append([centre_x, centre_y])
-    print(coordinates)
+    area = cv2.contourArea(contour)
+    if area > 50:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (25, 255, 255), 2)
+        centre_x, centre_y = ((x * 2 + w) // 2, (y * 2 + h) // 2)
+        coordinates.append([centre_x, centre_y])
+        count += 1
+
+print(coordinates)
+print("The number of ticks", count)
 
 result = cv2.resize(result, (360, 500))
+image = cv2.resize(image, (800, 1000))
 
 cv2.imshow('mask', mask)
 cv2.imshow('result', result)
+cv2.imshow('image', image)
 cv2.waitKey()
